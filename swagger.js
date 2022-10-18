5498597b5c7008161533639662eebf35112794f3
peterjwatson <peterskyguy@gmail.com>
2022-10-18 14:28:16 -0400

    swagger change

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Village Lane Publishing Books API',
         description: 'Village Lane Publishing Books',
     },
-    host: 'https://cse341node-books.onrender.com',
+    host: 'cse341node-books.onrender.com',
     schemes: ['http', 'https']
 };
 
